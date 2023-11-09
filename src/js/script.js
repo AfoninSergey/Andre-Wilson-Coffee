42ffc66370ede961cd2150fd27d2eb7794c307f8
Afonin Sergey <afoninsergey@mail.ru>
2023-11-09 14:41:07 +0300

    Replaced the favicon, Promo section, Backgound slider, first try

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".promo__background").slick({
+        arrows: false,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        fade: true,
+        cssEase: "linear",
+        slickPause: true
+    });
+});
