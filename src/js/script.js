cfc402d290a758b999258292c6130d2643a692d0
Afonin Sergey <afoninsergey@mail.ru>
2023-11-15 16:10:28 +0300

    Adapted the navigation,  added About section, adapted About

--- src/js/script.js
+++ src/js/script.js
@@ -11,13 +11,85 @@ $(document).ready(function () {
         cssEase: "linear",
         slickPause: true,
     });
+    //navigation adapt
+    const menu = document.querySelector(".navigation"),
+        menuItems = document.querySelectorAll(".navigation a"),
+        hamburger = document.querySelector(".hamburger");
+
     //navigation scroll
-    $(window).scroll(function () {        
-        if ($(this).scrollTop() > 300) {
-            // console.log($(this).scrollTop())
-            $(".navigation").addClass("navigation_scroll");
+    $(window).scroll(function () {       
+        if ($(this).scrollTop() > 200) {
+            $(".navigation").addClass("200");
         } else {
+            $(".navigation").removeClass("200");
+        }
+        
+        if ($(this).scrollTop() > 200 && !$(".navigation").hasClass("navigation_scroll")) { // добавил 15.11.2023. Надо убедиться           
+            $(".navigation").addClass("navigation_scroll");      
+        } else if ($(this).scrollTop() < 200 && !hamburger.classList.contains('hamburger_active')) {
+            $(".navigation").removeClass("navigation_scroll");            
+        }
+
+    });
+
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("hamburger_active");
+        menu.classList.toggle("navigation_active");
+        if ($(".navigation").hasClass("navigation_scroll") && !hamburger.classList.contains('hamburger_active')
+        && !$(".navigation").hasClass("200")
+        ) {
+            // console.log('Yes');
             $(".navigation").removeClass("navigation_scroll");
+        } else {
+            $(".navigation").addClass("navigation_scroll");
+            // console.log('No');
         }
     });
+    menuItems.forEach((i) => {
+        i.addEventListener("click", (e) => {
+            // menuItems.forEach((i) => {
+            //     i.classList.remove('active')  //Понял как сделать ссылку активной после нажатия, но не понял, как правильно удалить
+            // })            
+            // e.target.classList.add('active')
+            hamburger.classList.remove("hamburger_active");
+            menu.classList.remove("navigation_active");
+            if ($(".navigation").hasClass("navigation_scroll") && !hamburger.classList.contains('hamburger_active')
+            && !$(".navigation").hasClass("200")
+        ) {
+            // console.log('Yes');
+            $(".navigation").removeClass("navigation_scroll");
+        }
+        });
+    });
+    // Ниже скрипт делает плавный скролл
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 600, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+    // Ниже рабочий (кажется) вариант подсветки соответстующей блоку ссылки
+// jQuery(window).scroll(function(){
+//     var $sections = $('section');
+// $sections.each(function(i,el){
+//    var top  = $(el).offset().top-100;
+//    var bottom = top +$(el).height();
+//    var scroll = $(window).scrollTop();
+//    var id = $(el).attr('id');
+//    if( scroll > top && scroll < bottom){
+//        $('a.active').removeClass('active');
+//        $('a[href="#'+id+'"]').addClass('active');
+
+//    }
+// })
+// });
+
+
+
 });
